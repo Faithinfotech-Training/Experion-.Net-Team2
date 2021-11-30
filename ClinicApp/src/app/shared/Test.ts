66fc56e5200a7567393cb4ba0ea584e9ff303737
Jyothish-A <jyothish.a@experionglobal.com>
2021-11-30 22:42:59 +0530

    .

---
+++ ClinicApp/src/app/shared/Test.ts
@@ -0,0 +1,13 @@
+export class Test {
+    testId: number;
+    testNo: number | null;
+    testDateTime: string | null;
+    testAmount: number | null;
+    range: string;
+    testDescription: string;
+    reportNo: number | null;
+    isactive: boolean | null;
+    result: number;
+    //reportNoNavigation: Labreport;
+    //testNoNavigation: Testdetails;
+}
