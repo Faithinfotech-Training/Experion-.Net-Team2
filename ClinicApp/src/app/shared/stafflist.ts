4f9660b744e51122393962fbad879d04dea9c0d9
abrahamannie <annie.abraham@experionglobal.com>
2021-12-02 00:54:34 +0530

    angular

---
+++ ClinicApp/src/app/shared/stafflist.ts
@@ -0,0 +1,13 @@
+export class StaffList{
+    StaffId:number;
+    StaffName:string;
+    RoleName:string;
+    Gender:string;
+    DateOfBirth:Date;
+    Address:string;
+    DateOfJoin:Date;
+    Mobile:string;
+    Experience:number;
+    Email:string;
+    Isactive:boolean;
+}
