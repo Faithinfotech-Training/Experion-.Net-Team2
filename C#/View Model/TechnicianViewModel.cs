90de1f641fdb17c653b56c7587fa7d0ea25d77ed
HaizonCruz <haizon.cruz@experionglobal.com>
2021-12-03 15:12:58 +0530

    Added C#

---
+++ C#/View Model/TechnicianViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSAPI.View_Model
+{
+    public class TechnicianViewModel
+    {
+        public int LabtechnicianId { get; set; }
+
+        public string StaffName { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public bool? Isactive { get; set; }
+    }
+}
